9b015d20ee5827c01da685d8da9259892420a28e
unknown <saravananpushparaj@gmail.com>
2015-10-23 10:48:56 +0530

    V1.0

---
+++ src/test/java/locat.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+
+public class locat {
+	
+	@Test(groups={"SmokeTest"})
+	
+	public void first_test()
+	{
+		
+		System.out.println("Hello world");
+		System.out.println("Welcome");
+		
+		
+		
+	}
+
+}
